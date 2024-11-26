2374989891ab201bbd2bb568057389840f43f79e
kwonkiyong0059 <kkoung425@gmail.com>
2024-11-26 11:32:33 +0900

    work

--- src/main/java/com/kkd/SimpleCalculator.java
+++ src/main/java/com/kkd/SimpleCalculator.java
@@ -3,15 +3,6 @@ package com.kkd;
 public class SimpleCalculator {
 
     public int plus(int num1, int num2) {
-
-        if(num1 == 2 && num2 == 10){
-            return 12;
-        }
-
-        if(num1 == 20 && num2 == 10){
-            return 30;
-        }
-
-        return 3;
+        return num1 + num2;
     }
 }
